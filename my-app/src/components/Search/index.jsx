2a5de67bbdb6a450cc8ec5fb6d2437fa01f9c2a3
linkSofuny <cyd551646@sina.com>
2021-05-13 09:46:01 +0800

    axios

--- my-app/src/components/Search/index.jsx
+++ my-app/src/components/Search/index.jsx
@@ -1,11 +1,20 @@
 import './index.css'
 import React, { Component } from 'react'
+import axios from 'axios'
 export default class Search extends Component {
+    constructor () {
+        super()
+        this.searchHandle = this.searchHandle.bind(this)
+    }
+    searchHandle () {
+        console.log(this.keyWordElement.value);
+    }
     render() {
         return (
             <header className="search-box">
                 search user by name<br />
-                <input type="text"/>
+                <input type="text" placeholder="输入关键字搜" ref={ c => this.keyWordElement = c }/>
+                <button onClick={this.searchHandle}>search</button>
             </header>
         )
     }
