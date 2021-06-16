7406bf05d8074f36c33dc2568710ab80a6c16e61
linkSofuny <cyd551646@sina.com>
2021-06-16 12:45:54 +0800

    redux-demo-finish

---
+++ redux-demo/src/redux/count_action.js
@@ -0,0 +1,7 @@
+
+/**
+ *  Action
+*/
+import {INCREMENT, DECREMENT} from './constant'
+export const incrementAction = data => ({type: INCREMENT, data})
+export const decrementAction = data => ({type: DECREMENT, data})
