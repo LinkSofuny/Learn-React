60e94ad08c7d3338347fe07a0ae68258160c356a
linkSofuny <cyd551646@sina.com>
2021-06-19 13:29:37 +0800

    redux-finish

--- Demo/redux_数据共享/redux/store.js
+++ Demo/redux_数据共享/redux/store.js
@@ -3,11 +3,10 @@ import countReducer from './reducers/count-reducers'
 import personReducers from "./reducers/person";
 import thunk from 'redux-thunk'
 
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const allReducer = combineReducers({
     count: countReducer,
     persons: personReducers
 })
 
-export default createStore(allReducer, composeWithDevTools(applyMiddleware(thunk)))
+export default createStore(allReducer, applyMiddleware(thunk))
