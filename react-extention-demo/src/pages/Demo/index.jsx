765e0f795ad86de000eba30c23ffcf15382d3c50
linkSofuny <cyd551646@sina.com>
2021-06-21 13:06:48 +0800

    useRef-demo

--- react-extention-demo/src/pages/Demo/index.jsx
+++ react-extention-demo/src/pages/Demo/index.jsx
@@ -1,10 +1,15 @@
+import { func } from 'prop-types'
 import React from 'react'
 import ReactDom from 'react-dom'
-// class Demo extends React.Component {
+// export default class Demo extends React.Component {
+//     myRef = React.createRef()
+//     showInfo = () => {
+//         console.log(this.myRef);
+//     }
 //     render() {
 //         return (
 //             <div>
-//                 Demo
+//                 <input type="text" ref={this.myRef} onChange={this.showInfo}/>
 //             </div>
 //         )
 //     }
@@ -12,26 +17,29 @@ import ReactDom from 'react-dom'
 export default function Demo () {
 
     const [count, setCount] = React.useState(0)
-
+    const myRef = React.useRef()
     
-    React.useEffect(() => {
-        console.log(1);
-        let timer =setInterval(() => {
-           add()
-        },1000)
-        return () => {
-            clearInterval(timer)
-        }
-    },[])
+    // React.useEffect(() => {
+    //     console.log(1);
+    //     let timer =setInterval(() => {
+    //        add()
+    //     },1000)
+    //     return () => {
+    //         clearInterval(timer)
+    //     }
+    // },[])
     function unmout () {
         ReactDom.unmountComponentAtNode(document.getElementById('root'))
     }
     function add () {
         setCount( count => count + 1)
     }
-
+    function showInfo() {
+        console.log(myRef.current.value);
+    }
     return (
         <div>
+            <input type="text" ref={myRef} onChange={showInfo}/>
             <h1>Demo: {count}</h1>
             <button onClick={add}>click</button>
             <button onClick={unmout}>unmout</button>
