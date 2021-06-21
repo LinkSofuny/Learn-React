c8764ac876805e0b3294f9adbfc1c80b8d957fc1
linkSofuny <cyd551646@sina.com>
2021-06-21 14:01:05 +0800

    fragment

---
+++ react-extention-demo/src/pages/DemoFrag/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component,Fragment } from 'react'
+
+export default class DemoFrag extends Component {
+    render() {
+        return (
+            <Fragment>
+               <p>111</p>
+            </Fragment>
+        )
+    }
+}
