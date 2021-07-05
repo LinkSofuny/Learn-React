f32ea73ef394369bb6630c673477de21767bcf62
linkSofuny <cyd551646@sina.com>
2021-07-05 13:40:05 +0800

    React-Router

--- react-extention-demo/src/index.js
+++ react-extention-demo/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter, HashRouter} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const getConfirmation = (message, callback) => {
+    const allowTransition = window.confirm(message)
+    callback(allowTransition)
+}
+ReactDOM.render(
+    <BrowserRouter getUserConfirmation={getConfirmation}>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'))
