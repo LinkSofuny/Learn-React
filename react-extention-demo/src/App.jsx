fcd2bd5a1aad8bdbc5b07ed4d6f860e488690e60
linkSofuny <cyd551646@sina.com>
2021-07-07 09:42:58 +0800

    router render

--- react-extention-demo/src/App.jsx
+++ react-extention-demo/src/App.jsx
@@ -3,6 +3,7 @@ import About from './pages/About'
 import Home from './pages/Home'
 import MyNavLink from './components/MyNavLink'
 import { Route,Link} from 'react-router-dom'
+import { FadingRouter } from './components/FadingRouter'
 import './App.css'
 class App extends Component {
 
@@ -21,8 +22,8 @@ class App extends Component {
         </nav>
         <div className='right'>
           {/* 路由切換 */}
-          <Route path='/about' component={About} />
-          <Route path='/home' component={Home} />
+          <FadingRouter path='/about' component={About} />
+          <FadingRouter path='/home' component={Home} />
         </div>
       </div>
     )
